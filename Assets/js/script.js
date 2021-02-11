700605bd59238a9c223cfef919b7f53dff20ecda
MK <mihaelak08@gmail.com>
2021-02-11 10:18:58 -0500

    Javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,114 @@
 // Global variables
 var generateBtn = document.querySelector("#generate");
-var timerEl = document.getElementById('countdown');
-
-
-
-
+var timerEl = document.getElementById("set-time");
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var questions1El = document.getElementById("questions1");
+var questions2El = document.getElementById("questions2");
+var wrapper = document.getElementById("wrapper");
+var currentQuestion = 0;
+var interval;
 // GIVEN I am taking a code quiz
 
 // WHEN I click the start button
 
+generateBtn.addEventListener("click",function(){
+setTime();
+askQuestion();
+})
+
+  
+
 // THEN a timer starts and I am presented with a question
 // Timer that counts down from 30
-function countdown() {
-    var timeLeft = 30;}
+function setTime() {
+    console.log("set time")
+    var timeLeft = 60;
+    interval = setInterval(function(){
+        timeLeft--;
+        timerEl.textContent = "Time Left: " + timeLeft
+        if (timeLeft < 1){
+            clearInterval(interval);
+            alert("Sorry, your time is up!")
 
-    function askquestion(){
-        var question = prompt("What do you always need in a function?")
+        }
+    }, 1000)
+}
+    
+
+    function askQuestion(){
+      var answers = document.getElementById("answers")
+        questionsEl.textContent = myQuestions[currentQuestion].question
+        wrapper.append(questionsEl)
+        
+        myQuestions[currentQuestion].choices.forEach(function(choices){
+            console.log(choices)
+            var answerButton = document.createElement("button")
+        answerButton.textContent = choices
+        answerButton.addEventListener("click",function(event){
+          console.log(event.target)
+            if (myQuestions[currentQuestion].answer === event.target.textContent){
+              alert ("Correct!")
+            }
+            else {
+            alert ("Wrong!")
+            }
+            currentQuestion++
+            answers.innerHTML = ""
+            if (currentQuestion === myQuestions.length){
+              gameOver()
+            }
+            else {
+              askQuestion()
+            }
+            
+        })
+        answers.append(answerButton)
+        })
     }
 
+    function gameOver (){
+      alert ("Game Over!");
+      clearInterval(interval)
+    }
+    
+
+
+    
+
 // WHEN I answer a question
+
+const myQuestions = [
+    {
+      question: "In which section do you link your CSS document to your HTML?",
+      choices: ["Doesn't Matter", "Head", "Body"],
+        answer: "Head",
+
+        
+      
+      },
+      
+  
+    {
+      question: "Which document styles your page?",
+      choices: ["HTML", "CSS", "Javascript"],
+        answer: "CSS",
+      
+      
+    },
+
+    {
+      question: "Which brackets do you use for an array?",
+      choices: ["()", "{}", "[]"],
+        answer: "[]",
+        
+      },
+    
+    
+  ];
+  
+  
+  
 
 // THEN I am presented with another question
 
